50023336b1ffe57735f3267c38bbdeb63848b592
Irina Novikova <nvkvirene@gmail.com>
2022-10-04 16:00:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AQA-Postman-Echo
+[![Build status](https://ci.appveyor.com/api/projects/status/sneum4iwq50gf4qf?svg=true)](https://ci.appveyor.com/project/nvkvirene/aqa-postman-echo)
---
+++ src/test/java/ru/netology/PostmanEchoTest2.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest2 {
+    @Test
+    public void shouldPostText() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("всё будет хорошо") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("всё будет хорошо"))
+        ;
+    }
+}
